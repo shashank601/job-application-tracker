4483c4c948312ddea3a7ef69f55c89603a9f60c3
Shashank <shashank3fr@gmail.com>
2026-06-11 22:06:24 +0530

    add Application and auth schema

---
+++ backend/src/controllers/auth_controller.js
@@ -0,0 +1,12 @@
+import User from "../models/user_model.js";
+
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    
+    
+  } catch (error) {
+    
+  }
+};
