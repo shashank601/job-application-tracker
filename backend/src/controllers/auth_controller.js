b2adf79ab32a8c52c995bb73ac1151b8d9bf567e
Shashank <shashank3fr@gmail.com>
2026-06-11 23:23:41 +0530

    add register controller

--- backend/src/controllers/auth_controller.js
+++ backend/src/controllers/auth_controller.js
@@ -1,12 +1,32 @@
 import User from "../models/user_model.js";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+        
+    const new_user = await User.create({ name, email, password });
+    
+    const token = jwt.sign({ id: new_user._id, role: new_user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res.status(201).json({
+      message: "User created successfully",
+      token,
+      user: {
+        id: new_user._id,
+        name: new_user.name,
+        email: new_user.email
+      }
+    });
     
     
   } catch (error) {
-    
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
