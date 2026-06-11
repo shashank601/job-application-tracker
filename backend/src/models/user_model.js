3445b1e016e07ff7569dcf419ab54686dbaed99d
Shashank <shashank3fr@gmail.com>
2026-06-11 21:00:40 +0530

    Add User and Application models

--- backend/src/models/user_model.js
+++ backend/src/models/user_model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 
 const user_schema = new mongoose.Schema(
   {
@@ -28,5 +28,13 @@ const user_schema = new mongoose.Schema(
 );
 
 
+user_schema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+    
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
 
 export default mongoose.model("User", user_schema);
