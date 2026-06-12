1e82eab3812fd31ba10829b79553035abe95339d
Shashank <shashank3fr@gmail.com>
2026-06-12 10:49:54 +0530

    add server and connectdb

---
+++ backend/src/server.js
@@ -0,0 +1,58 @@
+import express from "express";
+import routes from "./routes/routes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import error_handler from "./middlewares/error_handler.js";
+
+dotenv.config();
+
+const requiredEnvVars = [
+  "JWT_SECRET",
+  "JWT_EXPIRES_IN",
+  "MONGO_URL",
+  "PORT",
+  "CLIENT_URL",
+  "LOG_LEVEL"
+];
+
+requiredEnvVars.forEach((key) => {
+  if (!process.env[key]) {
+    throw new Error(`Missing environment variable: ${key}`);
+  }
+});
+
+
+
+
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(routes);
+app.use(error_handler);
+
+
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server started on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error("DB connection failed", err);
+    process.exit(1);
+  }
+};
+
+startServer();
