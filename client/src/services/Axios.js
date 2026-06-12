1b49d50394761265f1213fa07e16eb99f58f68a5
Shashank <shashank3fr@gmail.com>
2026-06-12 19:45:57 +0530

    add env examples

--- client/src/services/Axios.js
+++ client/src/services/Axios.js
@@ -1,8 +1,11 @@
 import axios from "axios";
+import dotenv from "dotenv";
 import { getToken } from "../utils/Token.js";
 
+dotenv.config();
+
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: process.env.BASE_URL,
 });
 
 
